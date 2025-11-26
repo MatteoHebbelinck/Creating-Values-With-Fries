670c72490b776db91f54230c217071e4ff0e7c89
Stan Van den heuvel <vandenheuvelstan@hotmail.com>
2025-11-26 12:56:25 +0100

    contacs

---
+++ components/specificComponents/Contact/Contact.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+export default class Contact extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() { 
+        return (
+            <div>                
+                Contact: {this.props.blok.title}
+            </div>    
+        );
+    }
+}
+
+
