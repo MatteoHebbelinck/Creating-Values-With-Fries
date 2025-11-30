46eb86a5f4768f2af08c97ad2534ae037ac296c9
MatteoHebbelinck <matteo.hebbelinck@ugent.be>
2025-11-30 17:09:33 +0100

    colorcode toevoegen aan app.js

--- pages/_app.js
+++ pages/_app.js
@@ -28,6 +28,7 @@ import OneCol from "../components/layoutComponents/OneCol/OneCol";
 import TwoCol from "../components/layoutComponents/TwoCol/TwoCol";
 import ThreeCol from "../components/layoutComponents/ThreeCol/ThreeCol";
 import Teaser from "../components/layoutComponents/Teaser/Teaser";
+import Colorcode from "../components/layoutComponents/Colorcode";
 
 
 // Remaining specific components
