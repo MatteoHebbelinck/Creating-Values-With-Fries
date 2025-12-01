160fb4fd66dfa3acfa5d735131d747546cefd4b3
landermussely-hash <landermussely@gmail.com>
2025-12-01 17:34:49 +0100

    colorcode in leftright blok gefixed

--- components/genericComponents/LeftRightBlock/LeftRightBlock.js
+++ components/genericComponents/LeftRightBlock/LeftRightBlock.js
@@ -1,28 +1,39 @@
 import React from "react";
-import { storyblokEditable } from "@storyblok/react";
+import { storyblokEditable } from "@storyblok/react";   // ⭐ TERUG TOEVOEGEN
 import css from "./LeftRightBlock.module.scss";
 import { getDefaultStoryBlokImageSet } from "../../../functions/StoryBlokImageHelper";
 import { RichTextToHTML } from "../../../functions/storyBlokRichTextRenderer";
 
 export default function LeftRightBlock({ blok }) {
-  let cssDirection = blok.direction === "imageright" ? "--reverse" : "";
-  let size = blok.forWithPageNavigator ? "--with-navigator" : "";
-  let cssColorBg = "--" + blok.colorcode.content.title;
-  let cssColorFont = "--highlighted-" + blok.colorcode.content.title;
+  const cssDirection = blok.direction === "imageright" ? "--reverse" : "";
+  const size = blok.forWithPageNavigator ? "--with-navigator" : "";
+
+  const colorEntry = Array.isArray(blok.colorcode)
+    ? blok.colorcode[0]
+    : blok.colorcode;
+
+  const colorName = colorEntry?.content?.title || "default";
 
   return (
-    <section
-      {...storyblokEditable(blok)}
+  <section
+    {...storyblokEditable(blok)}
+    className={[
+      css["highlighted-section"],
+      css[`highlighted-section--${colorName}`], // ⭐ kleur op wrapper
+    ].join(" ")}
+  >
+    <div
       className={[
-        css["highlighted-content" + size],
         css["highlighted-content"],
+        css["highlighted-content" + size],
       ].join(" ")}
     >
       {/* LEFT IMAGE / RIGHT TEXT */}
       <div
         className={[
-          css["highlighted-content__image-container" + cssDirection + size],
-          css["highlighted-content__image-container" + cssColorBg],
+          css["highlighted-content__image-container"],
+          css[`highlighted-content__image-container${cssDirection}`],
+          css[`highlighted-content__image-container${size}`],
         ].join(" ")}
       >
         <div className={css["highlighted-content__image-responsive-wrapper"]}>
@@ -36,24 +47,25 @@ export default function LeftRightBlock({ blok }) {
         </div>
       </div>
 
-      {/* RIGHT TEXT SECTION */}
-      <div className={css["highlighted-content__text-container" + cssDirection + size]}>
-
-        {/* ⭐ NEW: Title boven de richtext */}
+      {/* TEXT */}
+      <div
+        className={[
+          css["highlighted-content__text-container"],
+          css[`highlighted-content__text-container${cssDirection}`],
+          css[`highlighted-content__text-container${size}`],
+        ].join(" ")}
+      >
         {blok.title && (
-          <h2 className={css["highlighted-content__title"]}>
-            {blok.title}
-          </h2>
+          <h2 className={css["highlighted-content__title"]}>{blok.title}</h2>
         )}
 
-        {/* Original RichText */}
         {RichTextToHTML({
           document: blok.text,
-          textClassName: css["highlighted-content__large-text" + size],
-          boldClassName:
-            css["highlighted-content__large-text" + cssColorFont + size],
+          textClassName: css[`highlighted-content__large-text${size}`],
+          boldClassName: css["highlighted-content__large-text-bold"], // optioneel
         })}
       </div>
-    </section>
-  );
+    </div>
+  </section>
+);
 }
--- components/layoutComponents/Colorcode/Colorcode.js
+++ components/layoutComponents/Colorcode/Colorcode.js
@@ -2,9 +2,6 @@ import React from "react";
 import { storyblokEditable } from "@storyblok/react";
 
 export default function Colorcode({ blok }) {
-  return (
-    <div {...storyblokEditable(blok)}>
-      {blok.title}
-    </div>
-  );
+  // Dit component moet nooit visueel getoond worden.
+  return <div {...storyblokEditable(blok)} style={{ display: "none" }} />;
 }
