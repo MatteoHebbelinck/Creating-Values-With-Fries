186a573f3b3e9ec1f138438ca2657983d7506572
Stan Van den heuvel <vandenheuvelstan@hotmail.com>
2025-12-01 18:45:47 +0100

    con

--- components/specificComponents/Contact/Contact.js
+++ components/specificComponents/Contact/Contact.js
@@ -1,20 +1,12 @@
-import React, { Component } from "react";
+
 import { storyblokEditable } from "@storyblok/react"; 
 // Deze import is nodig om in de Storyblok editor te kunnen werken.
 
-export default class Contact extends Component {
-    
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        // Gebruik storyblokEditable om de component klikbaar/bewerkbaar te maken
-        return (
-            <div {...this.props.blok ? storyblokEditable(this.props.blok) : {}}>
-                {/* Gebruik this.props.blok.titel (kleine letters) volgens je Storyblok veldnamen */}
-                <h1>Contact: {this.props.blok.titel}</h1>
-            </div>
-        );
-    }
+export default function Contact({ blok }) {
+  return (
+    <div {...(blok ? storyblokEditable(blok) : {})}>
+      {/* Gebruik blok.titel (kleine letters) volgens je Storyblok veldnamen */}
+      <h1>Contact: {blok?.titel}</h1>
+    </div>
+  );
 }
