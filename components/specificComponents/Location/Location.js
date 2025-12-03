47c077aad03204e9597a41a5cd73e97fbfe282f4
Stan Van den heuvel <vandenheuvelstan@hotmail.com>
2025-12-03 11:08:06 +0100

    fix map

--- components/specificComponents/Location/Location.js
+++ components/specificComponents/Location/Location.js
@@ -11,13 +11,33 @@ export default class Location extends Component {
     super(props);
   }
 
+  // 🧠 Dit maakt automatisch een embed URL van een gewoon adres
+  generateEmbedFromAddress(address) {
+    if (!address) return null;
+
+    const encoded = encodeURIComponent(address);
+    return `https://www.google.com/maps?q=${encoded}&output=embed`;
+  }
+
   render() {
+    const blok = this.props.blok;
+
+    // 3 mogelijkheden:
+    // 1. mapUrl aanwezig → gebruik dat
+    // 2. address aanwezig → maak embed URL
+    // 3. geen van beide → toon geen map
+    const embedUrl = blok.mapUrl
+      ? blok.mapUrl
+      : blok.address
+        ? this.generateEmbedFromAddress(blok.address)
+        : null;
+
     return (
-      <div {...storyblokEditable(this.props.blok)}>
+      <div {...storyblokEditable(blok)}>
         <Headermenu blok={this.props.menu.content}></Headermenu>
 
         <main>
-          <Hero blok={this.props.blok} contentTypeTag="course" />
+          <Hero blok={blok} contentTypeTag="course" />
 
           <div className={css["location-page__main-content"]}>
             <div
@@ -28,28 +48,29 @@ export default class Location extends Component {
               <section className={css["rich-text-section--with-navigator"]}>
                 <h2 className={css["rich-text-section__title"]}>Location Details</h2>
                 <div className={css["rich-text-section__rich-text"]}>
-                  {RichTextToHTML({ document: this.props.blok.description })}
+                  {RichTextToHTML({ document: blok.description })}
                 </div>
               </section>
             </div>
 
-            {/* ✅ GOOGLE MAPS HIER TOEGEVOEGD */}
-            <div className={css["location-page__map"]} style={{ marginTop: "40px", textAlign: "center" }}>
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5017.11723708637!2d3.7247649765242845!3d51.04277234453954!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c37150c174667b%3A0x8f31c1404ab55e14!2sTweekerkenstraat%202%2C%209000%20Gent!5e0!3m2!1snl!2sbe!4v1764755403099!5m2!1snl!2sbe"
-                width="100%"
-                height="450"
-                style={{ border: 0, maxWidth: "800px" }}
-                allowFullScreen=""
-                loading="lazy"
-                referrerPolicy="no-referrer-when-downgrade"
-              ></iframe>
-            </div>
-            {/* ✅ EINDE GOOGLE MAPS */}
+            {/* ✅ GOOGLE MAP sectie die beide methodes ondersteunt */}
+            {embedUrl && (
+              <div className={css["location-page__map"]} style={{ marginTop: "40px", textAlign: "center" }}>
+                <iframe
+                  src={embedUrl}
+                  width="100%"
+                  height="450"
+                  style={{ border: 0, maxWidth: "800px" }}
+                  allowFullScreen=""
+                  loading="lazy"
+                  referrerPolicy="no-referrer-when-downgrade"
+                ></iframe>
+              </div>
+            )}
           </div>
 
-          {this.props.blok.additionalstuff &&
-            this.props.blok.additionalstuff.map((nestedBlok) => (
+          {blok.additionalstuff &&
+            blok.additionalstuff.map((nestedBlok) => (
               <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
             ))}
         </main>
