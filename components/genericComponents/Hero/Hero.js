9a0e30078b36ef94c8b860d9f9d62eb409224778
Stan Van den heuvel <vandenheuvelstan@hotmail.com>
2025-12-05 17:26:09 +0100

    hero layout

--- components/genericComponents/Hero/Hero.js
+++ components/genericComponents/Hero/Hero.js
@@ -2,42 +2,94 @@ import { storyblokEditable } from "@storyblok/react";
 import React from "react";
 import css from "./Hero.module.scss";
 import PropTypes from "prop-types";
-import { modifyStoryBlokImage } from "../../../functions/StoryBlokImageHelper";
-import "lazysizes";
-import "lazysizes/plugins/attrchange/ls.attrchange";
-import { RichTextToHTML } from "../../../functions/storyBlokRichTextRenderer"; 
-import { getDefaultStoryBlokImageSet } from "../../../functions/StoryBlokImageHelper";
-
+import { RichTextToHTML } from "../../../functions/storyBlokRichTextRenderer";
 
 export default function Hero({ blok }) {
-	let showImage = (blok.image && blok.image.filename);
-	let colorCssName = "--" + (blok.colorcode?.[0]?.content?.title || "");
-	let titleString = typeof blok.title === "string" ? blok.title : undefined;
-	let taglineString = typeof blok.tagline === "string" ? blok.tagline : undefined;
 
-	return (
-		<>
-			<div {...storyblokEditable(blok)} className={css["hero" + colorCssName]}>
-				<div className={[css["hero__container"], css["hero__container"]].join(" ")}>
-					<div className={[css["hero__title-group"], css["hero__title-group"]].join(" ")}>
-						<p className={css["hero__tag"]}>{blok.supertitle}</p>
-						<h1 className={css["hero__title"]}>{titleString || RichTextToHTML({ document: blok.title, textClassName: css["hero__title"], boldClassName: css["hero__title--highlighted" + colorCssName] })}</h1>
-						<div className={css["hero__subtitle"]}>{taglineString || RichTextToHTML({ document: blok.Tagline, textClassName: css["hero__subtitle"] })}</div>
-						{blok.tags && blok.tags.length > 0 && <TagList tags={blok.tags} variation={"white"} center />}
-					</div>
-					{showImage &&
-						<figure className={css["hero__image-responsive-wrapper"]}>
-							{getDefaultStoryBlokImageSet(blok.image.filename, blok.image.alt, { largestImageWidth: 870, largestImageHeigth: 870 }, 850, css["hero__image"])}
-						</figure>
-					}
+  /* ---------------------------
+     TEXT FIELDS
+  --------------------------- */
+  const titleString =
+    typeof blok.title === "string" ? blok.title : undefined;
 
-				</div>
-			</div>
-		</>
-		
-	);
+  const taglineString =
+    typeof blok.tagline === "string" ? blok.tagline : undefined;
+
+  const colorCssName = "--" + (blok.colorcode?.[0]?.content?.title || "");
+
+
+  /* ---------------------------
+     BACKGROUND IMAGE
+  --------------------------- */
+  const backgroundImage = blok.image?.filename
+    ? `url(${blok.image.filename})`
+    : "none";
+
+
+  /* ---------------------------
+     OVERLAY SETTINGS
+  --------------------------- */
+
+  // Convert % to 0–1
+  const overlayOpacity = (blok.overlay_opacity ?? 40) / 100;
+
+  // Storyblok single-option → hex map
+  const overlayColorMap = {
+    black: "#000000",
+    darkblue: "#001a33",
+  };
+
+  const overlayColor = overlayColorMap[blok.overlay_color] || "#000000";
+
+
+  /* ---------------------------
+     COMPONENT RENDER
+  --------------------------- */
+  return (
+    <section
+      {...storyblokEditable(blok)}
+      className={css["hero" + colorCssName]}
+      style={{
+        backgroundImage: backgroundImage,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        "--overlay-opacity": overlayOpacity,
+        "--overlay-color": overlayColor
+      }}
+    >
+
+      {/* GRADIENT OVERLAY (CSS handles gradient + opacity) */}
+      <div className={css["hero__overlay"]}></div>
+
+      {/* TEXT CONTENT */}
+      <div className={css["hero__container"]}>
+        <div className={css["hero__title-group"]}>
+
+          {blok.supertitle && (
+            <p className={css["hero__tag"]}>{blok.supertitle}</p>
+          )}
+
+          <h1 className={css["hero__title"]}>
+            {titleString ||
+              RichTextToHTML({
+                document: blok.title,
+                textClassName: css["hero__title"],
+                boldClassName:
+                  css["hero__title--highlighted" + colorCssName]
+              })}
+          </h1>
+
+          {taglineString && (
+            <div className={css["hero__subtitle"]}>{taglineString}</div>
+          )}
+        </div>
+      </div>
+
+    </section>
+  );
 }
 
 Hero.propTypes = {
-	blok: PropTypes.object
+  blok: PropTypes.object,
 };
